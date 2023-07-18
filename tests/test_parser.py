4159ab8f557a76d9bfe878005a7d93ea5896b5f3
Jake Waffle <2895324+jawaff@users.noreply.github.com>
2023-07-17 18:49:33 -0600

    Added a test for optional matchers

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -161,7 +161,6 @@ class TestParser(unittest.TestCase):
         parser.apply_token(7, results)
         self.assertEqual(parser.is_complete(), CompletionType.COMPLETE)
 
-
     def test_multiple_match_three_levels(self):
         rm1 = LiteralTokenMatcher(token_ids={1: None}, is_optional=False, matches_multiple=True)
         root_matchers = [rm1]
@@ -190,7 +189,6 @@ class TestParser(unittest.TestCase):
                 self.assertParseResultsEqual(results[2], MatchResult(is_valid=True, cur_rule_id=None, matched_token_ids=[1 for x in range(i+1)], matched_matcher_index=0, rule_completion=CompletionType.MAYBE_COMPLETE))
                 self.assertParseResultsEqual(results[3], NewRuleResult(is_valid=True, new_rule_id=1))
                 self.assertEqual(parser.is_complete(), CompletionType.MAYBE_COMPLETE)
-            # TODO This token is applied to an incorrect rule. Dunno what is going on.
             parser.apply_token(1, results)
 
             for j in range(2):
@@ -280,6 +278,96 @@ class TestParser(unittest.TestCase):
                 self.assertEqual(parser.is_complete(), CompletionType.MAYBE_COMPLETE)
                 parser.apply_token(7, results)
                 self.assertEqual(parser.is_complete(), CompletionType.MAYBE_COMPLETE)
+
+    def test_optional_three_levels(self):
+        # Iterates through token ids that should be left out.
+        for token_id_to_ignore in range(2, 8):
+            with self.subTest():
+                rm1 = LiteralTokenMatcher(token_ids={1: None}, is_optional=True, matches_multiple=False)
+                root_matchers = [rm1]
+                m1 = LiteralTokenMatcher(token_ids={2: None}, is_optional=True, matches_multiple=False)
+                m2 = LiteralTokenMatcher(token_ids={3: None}, is_optional=True, matches_multiple=False)
+                m3 = LiteralTokenMatcher(token_ids={4: None}, is_optional=True, matches_multiple=False)
+                m4 = LiteralTokenMatcher(token_ids={5: None}, is_optional=True, matches_multiple=False)
+                m5 = LiteralTokenMatcher(token_ids={6: None}, is_optional=True, matches_multiple=False)
+                m6 = LiteralTokenMatcher(token_ids={7: None}, is_optional=True, matches_multiple=False)
+                grammar = Grammar(root_rule=root_matchers, rules={1: [m1, m4], 2: [m2, m3], 5: [m5, m6]})
+                parser = TransformerParser(grammar)
+
+                # According to the grammar, this parser will accept the token sequence (regex notation used, kinda): (1, (2, 3?, 4?)?, (5, 6?, 7?)?)?.
+
+                self.assertEqual(parser.is_complete(), CompletionType.MAYBE_COMPLETE)
+                results = parser.test_token(1)
+                self.assertEqual(len(results), 2)
+                self.assertParseResultsEqual(results[0], MatchResult(is_valid=True, cur_rule_id=None, matched_token_ids=[1], matched_matcher_index=0, rule_completion=CompletionType.COMPLETE))
+                self.assertParseResultsEqual(results[1], NewRuleResult(is_valid=True, new_rule_id=1))
+                self.assertEqual(parser.is_complete(), CompletionType.MAYBE_COMPLETE)
+                parser.apply_token(1, results)
+                self.assertEqual(parser.is_complete(), CompletionType.MAYBE_COMPLETE)
+
+                tier_2_tokens = []
+                tier_A3_tokens = []
+                if token_id_to_ignore != 2:
+                    tier_2_tokens.append(2)
+                    results = parser.test_token(2)
+                    self.assertEqual(len(results), 2)
+                    self.assertParseResultsEqual(results[0], MatchResult(is_valid=True, cur_rule_id=1, matched_token_ids=tier_2_tokens, matched_matcher_index=0, rule_completion=CompletionType.MAYBE_COMPLETE))
+                    self.assertParseResultsEqual(results[1], NewRuleResult(is_valid=True, new_rule_id=2))
+                    self.assertEqual(parser.is_complete(), CompletionType.MAYBE_COMPLETE)
+                    parser.apply_token(2, results)
+
+                    if token_id_to_ignore != 3:
+                        tier_A3_tokens.append(3)
+                        results = parser.test_token(3)
+                        self.assertEqual(len(results), 1)
+                        self.assertParseResultsEqual(results[0], MatchResult(is_valid=True, cur_rule_id=2, matched_token_ids=tier_A3_tokens, matched_matcher_index=0, rule_completion=CompletionType.MAYBE_COMPLETE))
+                        self.assertEqual(parser.is_complete(), CompletionType.MAYBE_COMPLETE)
+                        parser.apply_token(3, results)
+
+                    if token_id_to_ignore != 4:
+                        tier_A3_tokens.append(4)
+                        results = parser.test_token(4)
+                        self.assertEqual(len(results), 1)
+                        self.assertParseResultsEqual(results[0], MatchResult(is_valid=True, cur_rule_id=2, matched_token_ids=tier_A3_tokens, matched_matcher_index=1, rule_completion=CompletionType.COMPLETE))
+                        self.assertEqual(parser.is_complete(), CompletionType.MAYBE_COMPLETE)
+                        parser.apply_token(4, results)
+
+                if token_id_to_ignore != 5:
+                    tier_2_tokens.append(5)
+                    results = parser.test_token(5)
+                    if token_id_to_ignore == 4:
+                        # token_id 4 is optional, so we get a false match before falling back to the next rule node.
+                        self.assertParseResultsEqual(results[0], MatchResult(is_valid=False, cur_rule_id=2, matched_token_ids=[3], matched_matcher_index=1, rule_completion=CompletionType.MAYBE_COMPLETE))
+                        self.assertParseResultsEqual(results[1], MatchResult(is_valid=True, cur_rule_id=1, matched_token_ids=tier_2_tokens, matched_matcher_index=1, rule_completion=CompletionType.COMPLETE))
+                        self.assertParseResultsEqual(results[2], NewRuleResult(is_valid=True, new_rule_id=5))
+                    else:
+                        self.assertEqual(len(results), 2)
+                        self.assertParseResultsEqual(results[0], MatchResult(is_valid=True, cur_rule_id=1, matched_token_ids=tier_2_tokens, matched_matcher_index=1, rule_completion=CompletionType.COMPLETE))                    
+                        self.assertParseResultsEqual(results[1], NewRuleResult(is_valid=True, new_rule_id=5))
+                    self.assertEqual(parser.is_complete(), CompletionType.MAYBE_COMPLETE)
+                    parser.apply_token(5, results)
+
+                    tier_B3_tokens = []
+                    if token_id_to_ignore != 6:
+                        tier_B3_tokens.append(6)
+                        results = parser.test_token(6)
+                        self.assertEqual(len(results), 1)
+                        self.assertParseResultsEqual(results[0], MatchResult(is_valid=True, cur_rule_id=5, matched_token_ids=tier_B3_tokens, matched_matcher_index=0, rule_completion=CompletionType.MAYBE_COMPLETE))
+                        self.assertEqual(parser.is_complete(), CompletionType.MAYBE_COMPLETE)
+                        parser.apply_token(6, results)
+
+                    if token_id_to_ignore != 7:
+                        tier_B3_tokens.append(7)
+                        results = parser.test_token(7)
+                        self.assertEqual(len(results), 1)
+                        self.assertParseResultsEqual(results[0], MatchResult(is_valid=True, cur_rule_id=5, matched_token_ids=tier_B3_tokens, matched_matcher_index=1, rule_completion=CompletionType.COMPLETE))
+                        self.assertEqual(parser.is_complete(), CompletionType.MAYBE_COMPLETE)
+                        parser.apply_token(7, results)
+                        self.assertEqual(parser.is_complete(), CompletionType.COMPLETE)
+                    else:
+                        self.assertEqual(parser.is_complete(), CompletionType.MAYBE_COMPLETE)
+                else:
+                    self.assertEqual(parser.is_complete(), CompletionType.MAYBE_COMPLETE)
 
 if __name__ == '__main__':
     unittest.main()
